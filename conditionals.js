5a20f74ef20586aaff8b052471bf645820d81e05
amanrawat1213 <69184589+amanrawat1213@users.noreply.github.com>
2025-04-23 18:03:36 +0530

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,40 @@
+let age = 18;
+console.log("aman");
+
+/*
+I am a 
+multiline 
+comment
+*/
+
+// if (age == 18) {
+//     console.log("You can drive");
+// }
+
+// else if (age == 0) {
+//     console.log("Are you kidding?")
+// }
+
+// else if (age == 1) {
+//     console.log("Are you again kidding?")
+// }
+
+// else {
+//     console.log("You cannot drive");
+// }
+
+// TERNARY OPERATORS
+// a = 6;
+// b = 8;
+// let c = a > b ? (a - b) : (b - a);
+
+/*
+translates to:
+if(a>b){
+    let c = a - b;
+}
+else {
+    let c = a - b;
+}
+
+*/
